d01869b07bde721286750e6c71815469d555f600
dinaldoap <dinaldoap@gmail.com>
2018-04-29 17:52:01 -0300

    move definição de bandwidth para do classificador ParzenWindowB

--- questao_2/parzen.py
+++ questao_2/parzen.py
@@ -1,19 +1,24 @@
 import numpy as np
-import sklearn.base as ba
 import statsmodels.api as sm
+import sklearn.base as ba
+from sklearn import model_selection as ms
+from sklearn.externals.joblib import memory as me
 
 class ParzenWindowB(ba.BaseEstimator, ba.ClassifierMixin):    
     """
     Parzen Window Bayes (ParzenWindowB)
     """
     def __init__(self, bandwidth=None):
-        self.bandwidth = bandwidth
+        self.bandwidth = bandwidth        
         
     def fit(self, X, y):
+        bandwidth = self.bandwidth
+        if bandwidth is None:
+            bandwidth = BandwidthOptimizer().best_bw(X, y)
         n_samples, n_vars = X.shape
         self.classes_ = np.sort(np.unique(y))
         training_sets = [X[y == yi] for yi in self.classes_]
-        self.models_ = [sm.nonparametric.KDEMultivariate(data=Xi, var_type='c'*n_vars, bw=[self.bandwidth]*n_vars)
+        self.models_ = [sm.nonparametric.KDEMultivariate(data=Xi, var_type='c'*n_vars, bw=[bandwidth]*n_vars)
                         for Xi in training_sets]
         return self
             
@@ -29,8 +34,20 @@ class ParzenWindowB(ba.BaseEstimator, ba.ClassifierMixin):
     def predict(self, X):
         return self.classes_[np.argmax(self.predict_proba(X), 1)]
 
-class WindowSizeOptimizer():
-    def bw(self, X):
-        n_samples, n_vars = X.shape
-        est = sm.nonparametric.KDEMultivariate(data=X, var_type='c'*n_vars, bw='normal_reference')
-        return est.bw
+class BandwidthOptimizer():
+    def __init__(self):
+        self.memory = me.Memory(cachedir='.cache', verbose=0)
+        
+    def best_bw(self, X, y):
+        best_bw_cached = self.memory.cache(_best_bw)
+        return best_bw_cached(self, X, y)
+    
+    def clear_cache(self):
+        self.memory.clear()
+                                           
+def _best_bw(bw_opt, X, y):        
+    # o grid de parâmetros abaixo define os possíveis valores da janela
+    param_grid = {'bandwidth': [3, 4, 5]}
+    grid_search = ms.GridSearchCV(ParzenWindowB(), param_grid=param_grid, cv=ms.StratifiedKFold(n_splits=5),return_train_score=False)
+    grid_search.fit(X, y)
+    return grid_search.best_params_['bandwidth']  
